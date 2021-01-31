29b58a9d12dd4f9649b396e89694a03663a64694
srikant vs <srikantvs26@gmail.com>
2021-01-31 07:19:33 +0530

    added code for knowing which beans are created at startup time.

--- user-service/src/main/java/com/srikantvs/user/config/UserConfig.java
+++ user-service/src/main/java/com/srikantvs/user/config/UserConfig.java
@@ -1,6 +1,8 @@
 package com.srikantvs.user.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,16 @@ public class UserConfig {
 	public RestTemplate restTemplate() {
 		RestTemplate restTemplate = new RestTemplate();
 		return restTemplate;
+	}
+	
+	@Bean
+	public CommandLineRunner runner(ApplicationContext context) {
+		String[] beanDefNames = context.getBeanDefinitionNames();
+		return args->{
+			for(String beanDefName: beanDefNames) {
+				System.out.println(beanDefName);
+			}
+		};
 	}
 
 }
